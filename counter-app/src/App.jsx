df677392f4e56e6bc9f7965cc71059e90c4d9dd5
Mykchailo <mishagazin85@gmail.com>
2024-10-31 00:42:53 +0200

    init

---
+++ counter-app/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import CounterDisplay from "./components/CounterDisplay.jsx";
+import CounterControls from "./components/CounterControls.jsx";
+
+
+function App() {
+return (
+    <>
+    <div className="App">
+        <h1>Лічильник з Context</h1>
+        <CounterDisplay />
+        <CounterControls />
+    </div>
+       </>
+);
+}
+
+export default App

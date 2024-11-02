2f2c255f3f2c44bef16d1e1776b611d1620df4d0
Mykchailo <mishagazin85@gmail.com>
2024-11-03 01:23:41 +0200

    3

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {CounterProvider} from "./context/CounterContext.jsx";
-import App from './App.jsx'
+import {CounterProvider} from "./context/CounterContext";
+import App from './App'
 import './styles/main.scss'
 
 
